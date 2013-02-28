4601f81dab52d4ca37a3c9893609f2bb95ca133b
ajlopez <webmaster@ajlopez.com>
2013-02-28 19:17:52 -0300

    game.play, first test and implementation

--- test/game.js
+++ test/game.js
@@ -17,3 +17,15 @@ var positions = game.getPositions();
 assert.ok(positions);
 assert.equal(positions.length, 0);
 
+// play black
+
+game.play(3, 3, simplego.Black);
+
+var positions = game.getPositions();
+
+assert.ok(positions);
+assert.equal(positions.length, 1);
+assert.equal(positions[0].x, 3);
+assert.equal(positions[0].y, 3);
+assert.equal(positions[0].content.color, simplego.Black);
+
