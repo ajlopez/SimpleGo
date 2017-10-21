5961b17747ca81842a1014f1d344936aef16baa6
ajlopez <webmaster@ajlopez.com>
2017-10-21 19:07:50 -0300

    Get string with only one stone

--- test/strings.js
+++ test/strings.js
@@ -7,3 +7,17 @@ exports['get string from empty cell'] = function (test) {
 	test.equal(game.string(3, 3), null);
 }
 
+exports['get string from cell with stone'] = function (test) {
+	var game = simplego.game();
+	
+	game.play(3, 3, simplego.Black)
+	
+	var string = game.string(3, 3);
+	
+	test.ok(string);
+	test.ok(string.cells);
+	test.ok(Array.isArray(string.cells));
+	test.equal(string.cells.length, 1);
+	test.deepEqual(string.cells[0], { x: 3, y: 3, color: simplego.Black });
+}
+
