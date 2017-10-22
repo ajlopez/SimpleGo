97578a784f2ffdaf6db5062b3044369e3836483b
ajlopez <webmaster@ajlopez.com>
2017-10-22 11:55:07 -0300

    Get string with two stones

--- test/strings.js
+++ test/strings.js
@@ -15,9 +15,21 @@ exports['get string from cell with stone'] = function (test) {
 	var string = game.string(3, 3);
 	
 	test.ok(string);
-	test.ok(string.stones);
-	test.ok(Array.isArray(string.stones));
-	test.equal(string.stones.length, 1);
-	test.deepEqual(string.stones[0], { x: 3, y: 3, color: simplego.Black });
+	test.equal(string.stones().length, 1);
+	test.deepEqual(string.stones()[0], { x: 3, y: 3, color: simplego.Black });
+}
+
+exports['get string from cell with stone and neighbor stone'] = function (test) {
+	var game = simplego.game();
+	
+	game.play(3, 3, simplego.Black)
+	game.play(4, 3, simplego.Black)
+	
+	var string = game.string(3, 3);
+	
+	test.ok(string);
+	test.equal(string.stones().length, 2);
+	test.deepEqual(string.stones()[0], { x: 3, y: 3, color: simplego.Black });
+	test.deepEqual(string.stones()[1], { x: 4, y: 3, color: simplego.Black });
 }
 
