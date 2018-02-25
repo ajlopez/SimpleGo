6d2350d11e1fb7321c755eb974c15fa2a48e3320
ajlopez <webmaster@ajlopez.com>
2018-02-25 12:16:15 -0300

    Board with initial position

--- test/board.js
+++ test/board.js
@@ -1,5 +1,8 @@
 
 var simplego = require('../');
+
+var Black = simplego.Black;
+var White = simplego.White;
     
 exports['create board'] = function (test) {
 	var board = simplego.board();
@@ -15,6 +18,23 @@ exports['get no positions from empty board'] = function (test) {
 
 	test.ok(positions);
 	test.equal(positions.length, 0);
+}
+
+exports['get positions from board with initial position'] = function (test) {
+	var board = simplego.board(19, 19, [
+        '. . .',
+        '. X O',
+        '. . X'
+    ]);
+    
+	var positions = board.positions();
+
+	test.ok(positions);
+	test.equal(positions.length, 3);
+    
+    test.deepEqual(board.get(1, 1), { x: 1, y:1, color: Black });
+    test.deepEqual(board.get(2, 1), { x: 2, y:1, color: White });
+    test.deepEqual(board.get(2, 2), { x: 2, y:2, color: Black });
 }
 
 exports['valid and invalid cells'] = function (test) {
