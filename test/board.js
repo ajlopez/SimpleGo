deb14c51f27b6590fc2ced4935b47b5a15bc199c
ajlopez <webmaster@ajlopez.com>
2020-12-19 09:47:01 -0300

    Create default board with initial position

--- test/board.js
+++ test/board.js
@@ -38,6 +38,26 @@ exports['get positions from board with initial position'] = function (test) {
     test.deepEqual(board.get(2, 2), { x: 2, y:2, color: Black });
 }
 
+exports['create default board with initial position'] = function (test) {
+	const board = simplego.board([
+        '. . .',
+        '. X O',
+        '. . X'
+    ]);
+    
+    test.equal(board.width(), 19);
+    test.equal(board.height(), 19);
+    
+	const positions = board.positions();
+
+	test.ok(positions);
+	test.equal(positions.length, 3);
+    
+    test.deepEqual(board.get(1, 1), { x: 1, y:1, color: Black });
+    test.deepEqual(board.get(2, 1), { x: 2, y:1, color: White });
+    test.deepEqual(board.get(2, 2), { x: 2, y:2, color: Black });
+}
+
 exports['valid and invalid cells'] = function (test) {
 	const board = simplego.board();
 	
